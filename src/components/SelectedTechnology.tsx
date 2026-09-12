8529b86fe41af9f77abfc1ed498ed7702ee5bef8
sojib402 <sojib241-15-402@diu.edu.bd>
2026-09-12 23:02:34 +0600

    try to work SelectedTechlogy

--- src/components/SelectedTechnology.tsx
+++ src/components/SelectedTechnology.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const SelectedTechnology = ({selectedTechnology}) => {
   console.log(selectedTechnology,'selectedTechnology')
+   if (selectedTechnology.length===0){
     return (
-      
     <div className="w-80 border rounded-xl p-5">
       
       <h2 className="text-xl font-bold">
-        Your Stack
+        Your Stack 
       </h2>
     
       <p className="text-gray-400">
@@ -19,8 +19,37 @@ const SelectedTechnology = ({selectedTechnology}) => {
       </div>
   
     </div>
+    )
+      }
+      else{
+return (
+    <div className="w-80 border rounded-xl p-5">
+      
+      <h2 className="text-xl font-bold">
+        Your Stack {selectedTechnology.length}
+      </h2>
+    
+      <p className="text-gray-400">
+        {selectedTechnology.length} technologies selected 
+      </p>
+       { 
+       selectedTechnology.map((technology)=>{
 
-    );
+         <div className="border border-dashed rounded-xl p-8 mt-5 text-center text-gray-400 flex">
+       <img src={technology.icon} className='w-16 h-16 object-contain'/>
+       <div>
+       <h2 className='font-bold text-2xl'>{technology.name}</h2>
+       <p>{technology.category}</p>
+      </div>
+      </div>
+       })
+     
+  }
+    </div>
+    )
+      
+      
+  } 
 };
 
 export default SelectedTechnology;
